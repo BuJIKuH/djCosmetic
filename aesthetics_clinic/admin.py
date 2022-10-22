75d49b87ae39d399939cdc2be5d06a090ef363b0
BuJIKuH <klassniy.jew@gmail.com>
2022-10-22 18:57:27 +0300

    add  in admin.py

--- aesthetics_clinic/admin.py
+++ aesthetics_clinic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProceduresModels, ProblemsModel, PartOfBodyModel, MastersModel
+
+admin.site.register(ProceduresModels)
+admin.site.register(ProblemsModel)
+admin.site.register(PartOfBodyModel)
+admin.site.register(MastersModel)
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import ProductsModel, CategoryLineModels
+
+admin.site.register(ProductsModel)
+admin.site.register(CategoryLineModels)
