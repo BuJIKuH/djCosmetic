2c41df6e68d01cd73e1ce14ccf26b537dbcbc966
BuJIKuH <klassniy.jew@gmail.com>
2022-10-22 15:14:06 +0300

    Функции вывода данных заменены на классы.

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 
 from news import views
 
 urlpatterns = [
-    path('news/', views.news_page, name='news'),
-    # path('', views.NewsView.as_view(), name='home'),
-    path('news/<str:slug>/', views.view_news, name='view_news'),
+    path('news/', views.NewsView.as_view(), name='news'),
+    path('news/<str:slug>/', views.NewsViewDetail.as_view(), name='view_news'),
 
 ]
